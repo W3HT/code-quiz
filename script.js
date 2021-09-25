afb55784b35abb2789fd7a2d528927060f82ff1c
Frost <mpyang2021@gmail.com>
2021-09-24 19:08:21 -0500

    Commit- errors in style.css

---
+++ script.js
@@ -0,0 +1,20 @@
+// selectors
+var option1 = document.
+// call global variables
+var secondsLeft = 60
+var questionIndex
+var 
+
+// arrays
+var question1 = {
+    questionIn
+}
+// function startGame
+// function startTimer
+// function load question
+    //event listener
+        // button pressed
+        // correct
+            //next question
+        // 
+        

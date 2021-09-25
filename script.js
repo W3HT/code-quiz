7555ef427a6787a8e8b3a5b4bb796275f20c27c9
Frost <mpyang2021@gmail.com>
2021-09-24 21:32:17 -0500

    updated div boxes and buttons

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
 // selectors
-var option1 = document.
+var startBtn = document.getElementById("startBtn")
+var questionContainerDiv = document.getElementById("questions-container")
+var currentQuestionIndex
 // call global variables
-var secondsLeft = 60
-var questionIndex
-var 
+var secondsLeft = 60;
+var questionElement = document.getElementById("question")
+var answerBtnElement = document.getElementById("optsion-btns")
+const questions = [
+    {
+        question: "What is an array?",
+        answers: [
+            {text: "An array is a special variable, which can hold more than one value at a time.", correct: true},
+            {text: "It's a fish", correct: false},
+            {text: "It's a fish", correct: false},
+            {text: "It's a fish", correct: false},
+
+        ]
+        
+    }
+]
+
+
+
+
 
 // arrays
-var question1 = {
-    questionIn
-}
+
 // function startGame
+// function startGame () {
+   
+function startGame() {
+    console.log("YEET")
+    startBtn.classList.add("hide")
+    questionContainerDiv.classList.remove("hide")
+    currentQuestionIndex = 0
+    nextQuestion()
+
+
+    
+}
+
 // function startTimer
 // function load question
+
+function displayQuestion(questions) {
+    questionElement.innerText = question.question
+}
+
     //event listener
+    
+
+
+     
         // button pressed
         // correct
             //next question
+function nextQuestion() {
+
+}       
         // 
         
+startBtn.addEventListener('click', startGame)      

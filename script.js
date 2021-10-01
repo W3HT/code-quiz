1b9831c86553cde1916be76e3b2359c73fa84544
Frost <mpyang2021@gmail.com>
2021-09-30 20:31:19 -0500

    populating buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // selectors
 var startBtn = document.getElementById("startBtn");
 var questionContainerDiv = document.getElementById("questions-container");
-var questionAsk = document.getElementById("questionAsk")
+
 var currentQuestionIndex;
 // call global variables
 var secondsLeft = document.getElementById("secondsLeft");
@@ -10,11 +10,35 @@ var answerBtnElement = document.getElementById("optsion-btns");
 var scoreCounter = 0;
 var timer;
 var timerCount = 20;
+var questionAsk = document.getElementById("questionAsk")
 var answerA = document.getElementById("btn-1")
 var answerB = document.getElementById("btn-2")
 var answerC = document.getElementById("btn-3")
 var answerD = document.getElementById("btn-4")
+var nextQuestion;
+
+// arrays
 const questions = [
+    {
+        question: "What is an array?",
+        answers: ["A.  An array is a special variable, which can hold more than one value at a time.","B.  its a fish.", "C.  its a fruit","D.  its a banana"], answer: "A.  An array is a special variable, which can hold more than one value at a time."
+    },
+    {
+        question: "What is an array?",
+        answers: ["A.  An array is a special variable, which can hold more than one value at a time.","B.  its a fish.", "C.  its a fruit","D.  its a banana"], answer: "A.  An array is a special variable, which can hold more than one value at a time."
+    },
+    {
+        question: "What is an array?",
+        answers: ["A.  An array is a special variable, which can hold more than one value at a time.","B.  its a fish.", "C.  its a fruit","D.  its a banana"], answer: "A.  An array is a special variable, which can hold more than one value at a time."
+    },
+    {
+        question: "What is an array?",
+        answers: ["A.  An array is a special variable, which can hold more than one value at a time.","B.  its a fish.", "C.  its a fruit","D.  its a banana"], answer: "A.  An array is a special variable, which can hold more than one value at a time."
+    },
+    {
+        question: "What is an array?",
+        answers: ["A.  An array is a special variable, which can hold more than one value at a time.","B.  its a fish.", "C.  its a fruit","D.  its a banana"], answer: "A.  An array is a special variable, which can hold more than one value at a time."
+    },
     {
         question: "What is an array?",
         answers: ["A.  An array is a special variable, which can hold more than one value at a time.","B.  its a fish.", "C.  its a fruit","D.  its a banana"], answer: "A.  An array is a special variable, which can hold more than one value at a time."
@@ -25,43 +49,45 @@ const questions = [
 
 
 
-// arrays
+
 
 // function startGame
 // function startGame () {
    
 function startGame() {
-    startTimer();
+   
     console.log("YEET");
 
     startBtn.classList.add("hide");
     questionContainerDiv.classList.remove("hide");
     currentQuestionIndex = 0;
-    nextQuestion();
-
-
+  
+    startTimer();
     
 }
 
-// function startTimer
+// // function startTimer
 function startTimer() {
     timer = setInterval(function() {
     timerCount--;
     secondsLeft.textContent = timerCount;
     console.log(secondsLeft);
-    if (timerCount === 0) {
+        if (timerCount <= 0) {
         clearInterval(timer);
+        if(currentQuestionIndex < questions.length -1) {}
         score();
-            }
+        }
 
        
     }, 1000)
+
+    displayQuestion()
     
 }
 // function load question
 
-function displayQuestion(questions) {
-    currentQuestionIndex = 0;
+function displayQuestion() {
+    nextQuestion();
 }
 
     //event listener
@@ -73,19 +99,19 @@ function displayQuestion(questions) {
         // correct
             //next question
 function nextQuestion() {
-    questionAsk.textContent = questions[indexxxx].question;
-    answerA.textContent = questions[indexxxx].answers[0]
-    answerB.textContent = questions[indexxxx].answers[1]
-    answerC.textContent = questions[indexxxx].answers[2]
-    answerD.textContent = questions[indexxxx].answers[3]
-
-} 
-
-function score() {
-    scoreCounter++
-
-
+    questionAsk.textContent = questions[currentQuestionIndex].question;
+    answerA.textContent = questions[currentQuestionIndex].answers[0]
+    answerB.textContent = questions[currentQuestionIndex].answers[1]
+    answerC.textContent = questions[currentQuestionIndex].answers[2]
+    answerD.textContent = questions[currentQuestionIndex].answers[3]
 }
-        // 
+
+
+// function score() {
+//     scoreCounter++
+
+
+// }
+//         // 
         
-startBtn.addEventListener('click', startGame)      
+startBtn.addEventListener('click', startGame)

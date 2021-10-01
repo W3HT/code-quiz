1032c48d146b7ec8172bf8923f20f719476a1b21
Frost <mpyang2021@gmail.com>
2021-09-30 19:57:17 -0500

    updated question module and div box

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 // selectors
-var startBtn = document.getElementById("startBtn")
-var questionContainerDiv = document.getElementById("questions-container")
-var currentQuestionIndex
+var startBtn = document.getElementById("startBtn");
+var questionContainerDiv = document.getElementById("questions-container");
+var questionAsk = document.getElementById("questionAsk")
+var currentQuestionIndex;
 // call global variables
-var secondsLeft;
-var questionElement = document.getElementById("question")
-var answerBtnElement = document.getElementById("optsion-btns")
+var secondsLeft = document.getElementById("secondsLeft");
+
+var answerBtnElement = document.getElementById("optsion-btns");
 var scoreCounter = 0;
 var timer;
 var timerCount = 20;
+var answerA = document.getElementById("btn-1")
+var answerB = document.getElementById("btn-2")
+var answerC = document.getElementById("btn-3")
+var answerD = document.getElementById("btn-4")
 const questions = [
     {
         question: "What is an array?",
-        answers: [
-            {text: "An array is a special variable, which can hold more than one value at a time.", correct: true},
-            {text: "It's a fish", correct: false},
-            {text: "It's a fish", correct: false},
-            {text: "It's a fish", correct: false},
-
-        ]
-        
+        answers: ["A.  An array is a special variable, which can hold more than one value at a time.","B.  its a fish.", "C.  its a fruit","D.  its a banana"], answer: "A.  An array is a special variable, which can hold more than one value at a time."
     }
 ]
 
@@ -33,11 +31,13 @@ const questions = [
 // function startGame () {
    
 function startGame() {
-    console.log("YEET")
-    startBtn.classList.add("hide")
-    questionContainerDiv.classList.remove("hide")
-    currentQuestionIndex = 0
-    nextQuestion()
+    startTimer();
+    console.log("YEET");
+
+    startBtn.classList.add("hide");
+    questionContainerDiv.classList.remove("hide");
+    currentQuestionIndex = 0;
+    nextQuestion();
 
 
     
@@ -47,29 +47,21 @@ function startGame() {
 function startTimer() {
     timer = setInterval(function() {
     timerCount--;
-    timerElement = timerCount;
-    if (timerCount =< 0) {
+    secondsLeft.textContent = timerCount;
+    console.log(secondsLeft);
+    if (timerCount === 0) {
         clearInterval(timer);
         score();
             }
 
-        }
-    })
+       
+    }, 1000)
     
 }
 // function load question
 
 function displayQuestion(questions) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-    var button = document.createElement("button")
-    button.innerText = answer.text
-    button.classList.add("btn")
-    if(answer.correct) {
-        button.dataset.correct = answer.correct
-    }
-    button.addEventListener("click", )
-    })
+    currentQuestionIndex = 0;
 }
 
     //event listener
@@ -81,6 +73,11 @@ function displayQuestion(questions) {
         // correct
             //next question
 function nextQuestion() {
+    questionAsk.textContent = questions[indexxxx].question;
+    answerA.textContent = questions[indexxxx].answers[0]
+    answerB.textContent = questions[indexxxx].answers[1]
+    answerC.textContent = questions[indexxxx].answers[2]
+    answerD.textContent = questions[indexxxx].answers[3]
 
 } 
 
